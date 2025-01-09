3acc7bbc74367e2799f8301a37cb928ce9ea30b7
samdul-t1-AC-JeongMiEun <hahahello777@gmail.com>
2025-01-09 23:37:50 +0900

    Update name.py

--- src/chat/name.py
+++ src/chat/name.py
@@ -203,7 +203,7 @@ class Mammamia(App):
             bootstrap_servers=["ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092"],
             auto_offset_reset="earliest",
             #enable_auto_commit=True,
-            #group_id='chat_group',
+            group_id='chat_group',
             value_deserializer=lambda x: loads(x.decode('utf-8'))
         )
         try:
